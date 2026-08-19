4726492c9c31d6577182e0d5e7ee44a9e11f11bd
Navid Vafaei <vafaermn@gmail.com>
2026-08-19 16:00:31 +0330

    add base of unit test

---
+++ backend/HR.UnitTests/ImageServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace HR.UnitTests;
+
+public class ImageServiceTest
+{
+    [Fact]
+    public async void TestSaveImageAsync()
+    {
+        //assert
+        var env = new Mock<IWebHostEnvironment>();
+        env.Setup(x => x.ContentRootPath).Returns(Path.GetTempPath());
+        var service = new ImageService(env.Object);
+
+        //action
+        var stream = new MemoryStream();
+        var formFile = new FormFile(stream, 0, stream.Length, "file", "test.jpg");
+
+        var result = await service.SaveAsync(formFile);
+
+        //result
+        result.Should().NotBeNull();
+        result.Should().EndWith(".jpg");
+    }
+}
